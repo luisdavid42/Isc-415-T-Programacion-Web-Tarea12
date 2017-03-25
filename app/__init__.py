e2f9cb3ad88f36599a70f95d111fd195c003a84e
Luis David Miranda <ldmiranda123@gmail.com>
2017-03-25 01:30:40 -0400

    Leyendo Local Db Demo

--- app/__init__.py
+++ app/__init__.py
@@ -9,5 +9,8 @@ db.create_all(app=app)
 
 
 from app.controllers.main import main
-app.register_blueprint(main, url_prefix='/review')
-
+from app.controllers.movies import movies
+from app.controllers.reviews import reviews
+app.register_blueprint(main, url_prefix='/')
+app.register_blueprint(movies, url_prefix='/movies')
+app.register_blueprint(reviews, url_prefix='/reviews')
